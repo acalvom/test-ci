74119eb83bc11a8e3ff26e7ea43aad3bd155599e
acalvom <andreac.3108@gmail.com>
2021-01-29 12:16:13 +0100

    update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
+app.listen(port, function() {
+    console.log("Node Server at http://localhost:"+port);
+    console.log("Hora: " + Date());
 });
