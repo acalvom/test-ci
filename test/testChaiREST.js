09b89efff85e4cfe3283ebb3eae8e6c3f62bbfb0
acalvom <andreac.3108@gmail.com>
2021-02-02 18:23:54 +0100

    create test for getAll query

---
+++ test/testChaiREST.js
@@ -0,0 +1,20 @@
+'use strict';
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
+chai.use(chaiHttp);
+const url = 'http://localhost:8000';
+
+describe('get all personal', function () {
+    it('should get all personal', function (done) {
+        chai.request(url)
+            .get('/personal')
+            .end(function (err,res) {
+                console.log(res.body)
+                expect(res).to.have.status(200);
+                expect(res.body[0]).to.have.property('nombre','Fulanito');
+                done();
+                });
+    });
+})
