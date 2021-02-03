445c6f78e9467bb32ce886e1b3153c5f4b5004a2
acalvom <andreac.3108@gmail.com>
2021-02-03 13:13:55 +0100

    improve chai test

--- test/testChaiREST.js
+++ test/testChaiREST.js
@@ -1,7 +1,7 @@
 'use strict';
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-const expect = require('chai').expect;
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 const url = 'http://localhost:8000';
@@ -14,6 +14,7 @@ describe('get all personal', function () {
                 console.log(res.body)
                 expect(res).to.have.status(200);
                 expect(res.body[0]).to.have.property('nombre','Fulanito');
+                expect(res.body).to.be.a('array');
                 done();
                 });
     });
