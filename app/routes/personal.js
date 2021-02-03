c7c1f57acc933210b13bf5138224bf629974be5e
acalvom <andreac.3108@gmail.com>
2021-02-03 13:16:15 +0100

    create SELECT user by dni with GET & create SELECT user by dni with POST

--- app/routes/personal.js
+++ app/routes/personal.js
@@ -15,4 +15,32 @@ router.get('/personal', (req, res) => {
     })
 });
 
+router.get('/personal/:dni', (req, res) => {
+    var dni = req.params.dni;
+    var sql = 'SELECT * FROM personal WHERE dni = ?';
+    connection.query(sql, [dni], function (err, rows) {
+        if(err){
+            res.json(DBERROR);
+            console.log(err);
+        } else{
+            res.json(rows);
+        }
+    })
+});
+
+router.post('/personal/dni', (req, res) => {
+    //res.send("Post Query");
+    let dni = req.body.dni;
+    console.log(dni);
+    let sql = 'SELECT * FROM personal WHERE dni = ?';
+    connection.query(sql, [dni], function (err, rows) {
+        if(err){
+            res.json(DBERROR);
+            console.log(err);
+        } else{
+            res.json(rows);
+        }
+    })
+});
+
 module.exports = router;
