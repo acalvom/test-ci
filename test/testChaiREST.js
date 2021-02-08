7489d3a28dca6009be017cdb599aaf71ad94a91e
acalvom <andreac.3108@gmail.com>
2021-02-08 17:27:11 +0100

    add new tests

--- test/testChaiREST.js
+++ test/testChaiREST.js
@@ -6,9 +6,12 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 const url = 'http://localhost:8000';
 
+const toTest = require('../index');
+
 describe('get all personal', function () {
     it('should get all personal', function (done) {
-        chai.request(url)
+        //chai.request(url)
+        chai.request(toTest)
             .get('/personal')
             .end(function (err,res) {
                 console.log(res.body)
@@ -17,5 +20,22 @@ describe('get all personal', function () {
                 expect(res.body).to.be.a('array');
                 done();
                 });
+    });
+})
+
+describe('get a single person', function () {
+    it('should get a single person', function (done) {
+        //chai.request(url)
+        chai.request(toTest)
+            .get('/personal/111A')
+            .end(function (err,res) {
+                console.log(res.body)
+                expect(res).to.have.status(200);
+                expect(res.body[0]).to.have.property('nombre','Fulanito');
+                expect(res.body[0]).to.have.property('dni','111A');
+                expect(res.body[0]).to.have.property('edad',35);
+                expect(res.body[0].edad).to.be.a('number');
+                done();
+            });
     });
 })
