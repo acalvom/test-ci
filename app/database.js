27bfe8dae01ff5e00ca90eebd6c91fc3b74068b3
acalvom <andreac.3108@gmail.com>
2021-06-05 21:29:33 +0200

    update database.js

--- app/database.js
+++ app/database.js
@@ -3,46 +3,19 @@ const DBERROR = "Database Server Error";
 const DBOK = "Database Is Connected";
 
 var connection = mysql.createConnection({
-    // host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'miw_2020',
+    password: process.env.DB_PASSWORD || 'miw_2020',
+    database: process.env.DB_DATABASE || 'test'
 });
 
 // Check connection
 connection.connect(function (err){
     if(err){
-        console.log(err);
-        connection = reconnect(connection);
+        console.log(DBERROR);
     } else {
         console.log(DBOK);
     }
 });
-
-function reconnect(connection){
-    console.log("\n New connection tentative...");
-
-    //- Destroy the current connection variable
-    if(connection) connection.destroy();
-
-    //- Create a new one
-    var connection = mysql.createConnection({
-        // host: process.env.DB_HOST || 'localhost',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
-    });
-
-    //- Try to reconnect
-    connection.connect(function(err){
-        if(err) {
-            //- Try to connect every 2 seconds.
-            setTimeout(reconnect, 2000);
-        }else {
-            console.log("\n\t *** New connection established with the database. ***")
-            return connection;
-        }
-    });
-}
 
 module.exports = connection;
