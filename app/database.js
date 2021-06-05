3d46d4b739233d4e296d9700bdabf49c8649106b
acalvom <andreac.3108@gmail.com>
2021-06-05 20:24:51 +0200

    update database with env variables

--- app/database.js
+++ app/database.js
@@ -3,17 +3,16 @@ const DBERROR = "Database Server Error";
 const DBOK = "Database Is Connected";
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'miw_2020',
-    password: process.env.DB_PASSWORD || 'miw_2020',
-    database: process.env.DB_DATABASE || 'test'
+    // host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 // Check connection
 connection.connect(function (err){
     if(err){
         console.log(DBERROR);
-        return;
     } else {
         console.log(DBOK);
     }
